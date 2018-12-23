556c682a8e46296d83f5d40e3d9ded8b1f0e058f
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-12-23 13:34:19 -0800

    Express Simple REST

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -3,13 +3,71 @@ const http = require('http');
 
 // log incoming request on the screen
 // allow the browser to serve up static html files 
+// a middleware
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+// allows us to parse the body of the request in json format as req.body
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    //when a request comes in, for all the request, no matter
+    //what method invoked (get, put, post etc)
+    //for /dishes end point, these code will be executed first
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    // calling next() will continue to look for additional specification
+    // that matches '/dishes' endpoint
+    next();
+});
+
+// the above next() will invoke this function passing along req and res as arg
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req,res,next) => {
+    res.end('Will add the dish: ' + req.body.name + 
+    ' with details: ' + req.body.description);
+});
+
+// since put operation on /dishes doesn't make sense??
+// maybe it means not possible to edit all dishes at once?
+app.put('/dishes', (req,res,next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes');
+});
+
+/* for dishes/:dishId */
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'
+    + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' 
+    + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
 // __dirname means root folder
 app.use(express.static(__dirname + '/public'));
