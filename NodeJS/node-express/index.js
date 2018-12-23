c60ec28ec4b1490e2b9b0c617dd11556df3276b4
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-12-23 10:48:53 -0800

    Express serve static files with morgan

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const http = require('http');
 
+// log incoming request on the screen
+// allow the browser to serve up static html files 
+const morgan = require('morgan');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+// __dirname means root folder
+app.use(express.static(__dirname + '/public'));
 
 // next is an optional parameter - invoke additional middleware
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express server</h1></body><html>');
