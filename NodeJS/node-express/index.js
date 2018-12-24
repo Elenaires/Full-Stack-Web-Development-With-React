7eb73d4f0d4b8b355aeca7b51bc914f663f1bd74
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-12-23 17:44:41 -0800

    assignment 1

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -8,6 +8,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -20,27 +22,8 @@ app.use(bodyParser.json());
 // any request coming to /dishes end point will be
 // handled by dishRouter
 app.use('/dishes', dishRouter);
-
-/* for dishes/:dishId */
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/'
-    + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' 
-    + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // __dirname means root folder
 app.use(express.static(__dirname + '/public'));
