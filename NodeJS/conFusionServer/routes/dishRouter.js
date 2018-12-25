e10587a4cb61bfe51e010de7460a97ba31aa948d
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-12-25 14:47:50 -0800

    express generator REST API

---
+++ NodeJS/conFusionServer/routes/dishRouter.js
@@ -0,0 +1,65 @@
+/* this file will contain the implementing of handing of REST api
+* endpoint for /dishes and /dishes:dishId 
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+.all((req, res, next) => {
+    //when a request comes in, for all the request, no matter
+    //what method invoked (get, put, post etc)
+    //for /dishes end point, these code will be executed first
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    // calling next() will continue to look for additional specification
+    // that matches '/dishes' endpoint
+    next();
+})
+// the above next() will invoke this function passing along req and res as arg
+.get((req, res, next) => {
+    res.end('Will send all the dishes to you!');
+})
+.post((req,res,next) => {
+    res.end('Will add the dish: ' + req.body.name + 
+    ' with details: ' + req.body.description);
+})
+// since put operation on /dishes doesn't make sense??
+// maybe it means not possible to edit all dishes at once?
+.put((req,res,next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all the dishes');
+});
+
+
+
+/* for dishes/:dishId */
+dishRouter.route('/:dishId')
+.get((req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+})
+.post((req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'
+    + req.params.dishId);
+})
+.put((req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' 
+    + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+
+
+module.exports = dishRouter;
