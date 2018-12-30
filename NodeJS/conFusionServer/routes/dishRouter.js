88b4743899c05d9430f6f79326920ba41a072130
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-12-29 22:43:01 -0800

    Express REST API with MongoDB and Mongoose Part 1

--- NodeJS/conFusionServer/routes/dishRouter.js
+++ NodeJS/conFusionServer/routes/dishRouter.js
@@ -4,13 +4,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Dishes = require('../models/dishes');
 
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req, res, next) => {
+/*.all((req, res, next) => {
     //when a request comes in, for all the request, no matter
     //what method invoked (get, put, post etc)
     //for /dishes end point, these code will be executed first
@@ -20,14 +22,27 @@ dishRouter.route('/')
     // calling next() will continue to look for additional specification
     // that matches '/dishes' endpoint
     next();
-})
+})*/
 // the above next() will invoke this function passing along req and res as arg
 .get((req, res, next) => {
-    res.end('Will send all the dishes to you!');
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+    //res.end('Will send all the dishes to you!');
 })
 .post((req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + 
-    ' with details: ' + req.body.description);
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log("Dish Created ", dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 // since put operation on /dishes doesn't make sense??
 // maybe it means not possible to edit all dishes at once?
@@ -36,15 +51,25 @@ dishRouter.route('/')
     res.end('PUT operation not supported on /dishes');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all the dishes');
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
-
-
 
 /* for dishes/:dishId */
 dishRouter.route('/:dishId')
 .get((req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req,res,next) => {
     res.statusCode = 403;
@@ -52,14 +77,25 @@ dishRouter.route('/:dishId')
     + req.params.dishId);
 })
 .put((req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' 
-    + req.body.description);
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+        // will return the updated dish as json string in the reply
+    }, { new: true })
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .delete((req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
-
-
 
 module.exports = dishRouter;
